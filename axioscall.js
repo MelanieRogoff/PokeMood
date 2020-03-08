4797915e01d0eb84b0418c6b3794592a629dcf1f
Christopher Matthews <sirhunty@gmail.com>
2020-03-07 23:54:25 -0700

    updated style/format of html pages, also attempted to have choice display to pokemon html but did not work

--- axioscall.js
+++ axioscall.js
@@ -2,8 +2,9 @@ const axios = require('axios');
 
 const namer = 'pikachu'; //TESTING WITH JUST PIKACHU FOR NOW -- We will need to replace this with a this.name or something
 
+function pokeStuff() {
 axios.get(`https://pokeapi.co/api/v2/pokemon/${namer}`)
-  .then(response => { //call for the pokemon name and sprites
+  .then(response => showOutput(res)){ //call for the pokemon name and sprites
     const upper = response.data.name.charAt(0).toUpperCase() + response.data.name.substring(1);
     console.log(upper); //ensuring each Pokemon name is uppercase
     console.log(response.data.sprites.front_default)
@@ -13,11 +14,28 @@ axios.get(`https://pokeapi.co/api/v2/pokemon/${namer}`)
   });
 
   axios.get(`https://pokeapi.co/api/v2/pokemon-species/${namer}`) //Call for description
-  .then(response => {
+  .then(response => showOutput(res)) {
+
     console.log(response.data.flavor_text_entries[54].flavor_text);
   })
   .catch(error => {
     console.log(error);
   });
+}
+
+  // Show output in browser
+function showOutput(res) {
+  document.getElementById('res').innerHTML = `
+  <div class="card card-body mb-4">
+    <h5>Status: ${res.data.sprites.front_default}</h5>
+  </div>
+  <div class="card card-body mb-4">
+    <h5>Status: ${res.data.flavor_text_entries[54].flavor_text}</h5>
+  </div>
+`;
+}
+
+// Event listener
+document.getElementById('get').addEventListener('click', pokeStuff);
 
   
