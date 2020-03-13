ab3f1e7d233d476a660549161af4f8a931082f5a
Melanie Rogoff <mgrogoff@gmail.com>
2020-03-12 21:43:48 -0700

    Pushing

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,7 +1,5 @@
 // loginUser does a post to our "api/login" route and if successful, redirects us the the mood page
-console.log("test123")
 function loginUser(email, password) {
-    console.log(email, password);
     $.ajax({
       url: "/api/signup",
       method: 'POST',
@@ -12,7 +10,6 @@ function loginUser(email, password) {
   })
     .then(function() {
       window.location.replace("/mood");
-      // If there's an error, log the error
     })
     .catch(function(err) {
       console.log(err);
@@ -20,28 +17,28 @@ function loginUser(email, password) {
   }
   
   $(document).ready(function() {
-      // Getting references to our form and inputs
       const loginForm = $("#login-button");
       const emailInput = $("input#inputEmail4");
       const passwordInput = $("input#inputPassword4");
     
       // When the form is submitted, we validate there's an email and password entered
       loginForm.on("click", function(event) {
+          console.log("Is the button working")
         event.preventDefault();
         const userData = {
           email: emailInput.val().trim(),
           password: passwordInput.val().trim()
         };
-    
+        console.log(userData, "userData in Line 32")
         if (!userData.email || !userData.password) {
           return;
         }
     
         // If we have an email and password we run the loginUser function and clear the form
         loginUser(userData.email, userData.password);
+        console.log("Functioning", loginUser(userData.email, userData.password))
         emailInput.val("");
         passwordInput.val("");
-        console.log(userData.email, userData.password);
       });
     
     });
